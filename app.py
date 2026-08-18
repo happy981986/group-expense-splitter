dcd4f3df0b61b2944ee4d0624bd13e16249a97cc
happy981986 <happyjourneyhamesha@gmail.com>
2026-08-18 11:41:06 +0400

    Set preferred URL scheme to HTTPS

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 app.config["SESSION_COOKIE_SECURE"] = True
+app.config["PREFERRED_URL_SCHEME"] = "https"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
