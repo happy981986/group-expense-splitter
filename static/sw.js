4996f6e98932c6762a1021e316e94838c2c1c880
happy981986 <happyjourneyhamesha@gmail.com>
2026-08-18 11:47:03 +0400

    Refactor service worker to use ASSETS_TO_CACHE

--- static/sw.js
+++ static/sw.js
@@ -1,14 +1,23 @@
+// static/sw.js
 const CACHE_NAME = 'expense-splitter-v1';
-const urlsToCache = ['/', '/static/manifest.json'];
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/styles.css',
+  '/manifest.json'
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
